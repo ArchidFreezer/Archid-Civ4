88aa7679b31d37faf3187886b4d41fcf76c2d5d9
Archid <git@archid.org>
2022-12-19 22:14:46 +0000

    Add project base files
    Add the K-Mod source code and Platy UI python and XML files
    Add the Visual Studio project files, along with a Makefile to build the project
    
    Resolves: AC4-2

---
+++ CvGameCoreDLL/Archid.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifndef ARCHID_MOD_H
+#define ARCHID_MOD_H
+
+// name of the Python module where all the BUG functions that the DLL calls must live
+// MUST BE A BUILT-IN MODULE IN THE ENTRYPOINTS FOLDER
+// currently CvAppInterface
+#define PYBugModule				PYCivModule
+
+#define KMOD_NAME					L"K-Mod"
+#define KMOD_VERSION			L"1.44b"
+
+#define MOD_NAME				L"Archid"
+#define MOD_VERSION		L"0.1"
+#define MOD_BUILD			L"1"
+
+#endif
