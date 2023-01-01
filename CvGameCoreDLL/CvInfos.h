0faf575c2b3ad697b6c807f08cafcea41c3962e6
Archid <git@archid.org>
2023-01-01 12:02:43 +0000

    Rearrange member functions & variable declarations. No functional change
    AC4-30

--- CvGameCoreDLL/CvInfos.h
+++ CvGameCoreDLL/CvInfos.h
@@ -891,6 +891,8 @@ public:
 	int getNumPrereqVicinityOrBonus() const;
 	int getNumPrereqOrBuildingClasses() const;
 	int getNumPrereqNotBuildingClasses() const;
+	int getLeaderExperience() const;
+	int getLeaderPromotion() const;
 	int getMinPopulation() const;
 
 	bool isAnimal() const;				// Exposed to Python
@@ -1002,8 +1004,6 @@ public:
 	bool isPrereqVicinityOrBonus(BonusTypes eTerrain) const;
 	bool isPrereqOrBuildingClass(BuildingClassTypes eBuildingClass) const;
 	bool isPrereqNotBuildingClass(BuildingClassTypes eBuildingClass) const;
-	int getLeaderPromotion() const;   // Exposed to Python
-	int getLeaderExperience() const;				// Exposed to Python
 
 	const TCHAR* getEarlyArtDefineTag(int i, UnitArtStyleTypes eStyle) const;				// Exposed to Python
 	void setEarlyArtDefineTag(int i, const TCHAR* szVal);
@@ -1100,6 +1100,7 @@ protected:
 	int m_iNumUnitNames;
 	int m_iCommandType;
 	int m_iLeaderExperience;
+	int m_iLeaderPromotion;
 	int m_iMinPopulation;
 
 	bool m_bAnimal;
@@ -1142,7 +1143,6 @@ protected:
 	bool m_bNoRevealMap;
 	bool m_bSingleBuild;
 	bool m_bPrereqPower;
-	int m_iLeaderPromotion;
 
 	CultureLevelTypes m_eMinCultureLevel;
 	UnitRangeTypes 	m_eRangeType;
