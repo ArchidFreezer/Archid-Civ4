06401ae735038cc8e762bf241288371ddbac7f35
Archid <git@archid.org>
2023-01-05 16:17:41 +0000

    Added != operator to IDInfo
    Commit comment from K-Mod git repo
    
    AC4-129

--- CvGameCoreDLL/CvStructs.h
+++ CvGameCoreDLL/CvStructs.h
@@ -32,6 +32,7 @@ struct DllExport IDInfo {
 		return (eOwner == info.eOwner && iID == info.iID);
 	}
 
+	bool operator!= (const IDInfo& info) const { return !(*this == info); }
 	bool operator< (const IDInfo& a) const {
 		return eOwner < a.eOwner || (eOwner == a.eOwner && iID < a.iID);
 	}
