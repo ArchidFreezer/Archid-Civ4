5d06b756fc4dd73b582e5b831c9169a58669add4
Archid <git@archid.org>
2023-01-08 10:40:21 +0000

    Add Hurry automation
    Added a unit automation to hurry the construction of a building in an available city. The unit prioritises cities with a population of > 2/3 the player average, if there are no cities that match the population criteria then all cities are candidates. From the candidates the unit selects the one that will benefit the most given the time to get to the city.
    
    This automation is not available to GPs
    
    AC4-170

--- CvGameCoreDLL/CvUnit.cpp
+++ CvGameCoreDLL/CvUnit.cpp
@@ -2673,6 +2673,15 @@ bool CvUnit::canAutomate(AutomateTypes eAutomate) const {
 			return false;
 		break;
 
+	case AUTOMATE_HURRY:
+		if (m_pUnitInfo->getBaseHurry() <= 0)
+			return false;
+
+		//Do not give ability to great people
+		if (m_pUnitInfo->getProductionCost() < 0)
+			return false;
+		break;
+
 	default:
 		FAssert(false);
 		break;
