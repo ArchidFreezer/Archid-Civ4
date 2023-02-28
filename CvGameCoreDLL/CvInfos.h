2d05145453b6aec19af90b929299e7f3d4f5fe6b
Archid <git@archid.org>
2023-02-28 19:55:12 +0000

    Promotions allow settlers to spread state religion
    AC4-259

--- CvGameCoreDLL/CvInfos.h
+++ CvGameCoreDLL/CvInfos.h
@@ -584,6 +584,7 @@ public:
 	bool isCanMovePeaks() const;
 	bool isLoyal() const;
 	bool isSpyRadiation() const;
+	bool isCarryReligion() const;
 
 	const TCHAR* getSound() const;				// Exposed to Python
 	void setSound(const TCHAR* szVal);
@@ -697,6 +698,7 @@ protected:
 	bool m_bCanMovePeaks;
 	bool m_bLoyal;
 	bool m_bSpyRadiation;
+	bool m_bCarryReligion;
 
 	TechTypes m_eObsoleteTech;
 
